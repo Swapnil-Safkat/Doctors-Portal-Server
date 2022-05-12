2dba578d212b20a129a039e3e129a4ff42ef5b46
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-05-12 14:37:20 +0600

    'heroku'

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +10,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => { res.send('Doctors Portal Server Start') });
 
+const uri = `mongodb+srv://${process.env.DB_USER
+  }:${process.env.DB_PASS
+  }@learningmongo.qf50z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
 
-app.get('/', (req, res) => {res.send('Doctors Portal Server Start')})
-app.listen(port,()=>{console.log(`Running Doctors portal on port: ${port}`)})
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollections = client.db('DoctorsPortal').collection('services');
+
+    app.get('/services', async (req, res) => {
+      const cursor = servicesCollections.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+
+  }
+}
+
+run().catch(console.dir);
+app.listen(port, () => { console.log(`Running Doctors portal on port: ${port}`) });
